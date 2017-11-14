658107c93e7efee537e015d91c853aa813569996
Angelo Cigna <acigna1982@gmail.com>
2017-11-14 00:09:19 +0000

    automatically backed up by learn

--- question.js
+++ question.js
@@ -4,4 +4,17 @@ class Question {
  constructor(content) {
   this.content = content
   }
+
+  save(){
+    this.constructor.All().push(this)
+  }
+
+  static All() {
+    return this._All;
+  }
+  
+
+  
 }
+
+Question._All =[]
